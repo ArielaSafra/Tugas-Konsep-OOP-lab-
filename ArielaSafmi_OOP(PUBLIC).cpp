77dd804f4d47b0bc83459577b7a4544c96d655f0
riell <154486090+ArielaSafra@users.noreply.github.com>
2024-05-18 15:33:52 +0700

    Add files via upload

---
+++ ArielaSafmi_OOP(PUBLIC).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Deklarasi kelas
+class Cafe {
+public:
+    string nama; 
+    int umur;
+    string status;
+
+    // Metode untuk menampilkan informasi
+    void nama_pelanggan() {
+        cout << "Nama: " << nama << endl;
+        cout << "Umur: " << umur << endl;
+        cout << "Status: " << status << endl;
+    }
+};
+
+int main() {
+    // Membuat objek dari kelas Pelanggan
+    Pelanggan_cafe
+
+    // Memberi nilai atribut objek
+    pelanggan.nama = "Ola";
+    pelanggan.umur = 21;
+    pelanggan.status = "dine in";
+
+    // Memanggil metode untuk menampilkan informasi
+    pelanggan.nama_pelanggan();
+
+    return 0;
+}
